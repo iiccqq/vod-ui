3abe1f047c91c09cd243ebf7b4c379e9583f7fff
iiccqq <feng.chuang@163.com>
2018-04-01 11:47:22 +0800

    bb

--- page/js/vod/index.js
+++ page/js/vod/index.js
@@ -1,7 +1,7 @@
 var vm = new Vue({
   el: '#app',
   data: {
-	carouselItems:[{class:"carousel-item active",image:"data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",contentId:38},{class:"carousel-item",image:"data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",contentId:38},{class:"carousel-item",image:"data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",contentId:38}],
+	carouselItems:[{class:"carousel-item active",image:"data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",contentId:40},{class:"carousel-item",image:"data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",contentId:40},{class:"carousel-item",image:"data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==",contentId:40}],
 	items:[]
   },
   methods: {
@@ -12,8 +12,10 @@ var vm = new Vue({
   mounted:function(){
        jQuery.getJSON(baseURL + "index.do", function(r){
          for(var i= 0;i<vm.carouselItems.length;i++){
-           if(i<r.carousel.length)
+           if(i<r.carousel.length){
             vm.carouselItems[i].image = r.carousel[i].image;
+            vm.carouselItems[i].image = r.carousel[i].contentId;
+           }
          }
           vm.items = r.indexContent;
           for(var i=0;i<vm.items.length;i++){
